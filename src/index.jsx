f56023a1617d542e0bcdc2496e43312df71ade8d
Arthur Tapper <ArtyTapper@gmail.com>
2022-02-17 00:19:02 -0500

    Initial commit with some boilerplate templates

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.jsx';
-import './index.css';
+// styles provided by react grid layout library
+import "react-grid-layout/css/styles.css"
+import "react-resizable/css/styles.css"
+// my styles on top 
+import "./styles/gridlayout.css"
+
+import MyStaticGridLayout from './components/MyStaticGridLayout'
+import MyReactiveGridLayout  from './components/MyReactiveGridLayout' 
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <hr color="black" />
+    <MyStaticGridLayout />
+    <hr color="black" />
+    <MyReactiveGridLayout />
+    <hr color="black" />
   </React.StrictMode>,
   document.getElementById('root'),
 );
