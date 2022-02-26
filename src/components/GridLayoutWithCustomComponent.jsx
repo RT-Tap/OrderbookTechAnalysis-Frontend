69a5dc562f2759ed5bd657ab100fb9d940a717dc
Arthur Tapper <ArtyTapper@gmail.com>
2022-02-25 21:22:13 -0500

    save/refrence point before changing to use state&hooks for grid elements

--- src/components/GridLayoutWithCustomComponent.jsx
+++ src/components/GridLayoutWithCustomComponent.jsx
@@ -6,22 +6,22 @@ import CustomGridComponent from './CustomGridComponentWithSize-Template';
 // import CustomGridComponent from './CustomGridComponent-Template.jsx';
 
 const ReactiveGridItemSize = () => {
-    const items = ['One', 'Two', 'Three']
-    const layouts = {
+    const items = ['1', '2', '3']
+    const originalLayouts = {
         lg: [
-            { i: "One", x: 0, y: 0, w: 1, h: 2 },
-            { i: "Two", x: 1, y: 0, w: 3, h: 4, minW: 2, mxW: 4 },
-            { i: "Three", x: 4, y: 0, w: 1, h: 2 }
+            { i: "1", x: 0, y: 0, w: 1, h: 2 },
+            { i: "2", x: 1, y: 0, w: 3, h: 4, minW: 2, mxW: 4 },
+            { i: "3", x: 4, y: 0, w: 1, h: 2 }
         ],
         md: [
-            { i: "One", x: 0, y: 0, w: 1, h: 2 },
-            { i: "Two", x: 1, y: 0, w: 3, h: 4, minW: 2, mxW: 4 },
-            { i: "Three", x: 4, y: 0, w: 1, h: 2 }
+            { i: "1", x: 0, y: 0, w: 1, h: 2 },
+            { i: "2", x: 1, y: 0, w: 3, h: 4, minW: 2, mxW: 4 },
+            { i: "3", x: 4, y: 0, w: 1, h: 2 }
         ],
         sm: [
-            { i: "One", x: 0, y: 0, w: 1, h: 2 },
-            { i: "Two", x: 1, y: 0, w: 3, h: 4, minW: 2, maxW: 4 },
-            { i: "Three", x: 4, y: 0, w: 1, h: 2 }
+            { i: "1", x: 0, y: 0, w: 1, h: 2 },
+            { i: "2", x: 1, y: 0, w: 3, h: 4, minW: 2, maxW: 4 },
+            { i: "3", x: 4, y: 0, w: 1, h: 2 }
         ],
     }
 
@@ -29,7 +29,7 @@ const ReactiveGridItemSize = () => {
         let ls = {};
         if (localStorage) {
             try {
-                ls = JSON.parse(localStorage.getItem("gridLayout")) || {[key]: layouts};
+                ls = JSON.parse(localStorage.getItem("gridLayout")) || {[key]: originalLayouts};
             } catch (e) {
                 /*Ignore*/
             }
@@ -45,6 +45,15 @@ const ReactiveGridItemSize = () => {
     const [currentLayout, setCurrentLayout] = useState(getFromLS('layout'))
     const gridRef = createRef()
 
+    // react-grid-layout seems to default to large(est) breakpoint - intially(first load) will only call onBreakpointchnage for anything but the biggest size
+    const [currentBreakpoint, setCurrentBreakpoint] = useState({breakpoint: 'lg', columns: 12})
+
+    const onAddGridItem = () => {}
+
+    // this handles creating the actual 
+    const createGridItem = ([ ref=gridRef, key]) => {
+    }
+
     return (
         <div>
             <ResponsiveGridLayout
@@ -54,8 +63,9 @@ const ReactiveGridItemSize = () => {
                 breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
                 cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
                 rowHeight={30}
-                // onLayoutChange={(index, newlayout) => {console.log(index);console.log(newlayout)}}
+                // onLayoutChange={(currentLayout, allLayout) => {console.log(currentLayout);console.log(allLayout)}} // pretty sure tis returns current and all layouts
                 onLayoutChange={(key, newlayout) => {console.log('saving to ls');setCurrentLayout(newlayout); saveToLS("layout", newlayout)}}
+                onBreakpointChange={(breakpoint, cols) => {console.log('setting new breakpoint');setCurrentBreakpoint({breakpoint: breakpoint, columns: cols})}}
             >
                 {items.map((key) => <CustomGridComponent ref={gridRef} key={key} value={key}/>)}
             </ResponsiveGridLayout>
